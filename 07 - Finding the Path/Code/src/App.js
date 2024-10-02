9fa751000dc8d5c173bb36b199f9a481fd0296b5
Aviansh Ganpat Markad <100151747+AvinashMarkad@users.noreply.github.com>
2024-10-02 23:54:54 +0530

    adding components

--- 07 - Finding the Path/Code/src/App.js
+++ 07 - Finding the Path/Code/src/App.js
@@ -52,14 +52,7 @@ const appRouter = createBrowserRouter([
     ],
     errorElement: <Error />,
   },
-  // {
-  //   path: '/about',
-  //   element: <About />,
-  // },
-  // {
-  //   path: '/contact',
-  //   element: <Contact />,
-  // },
+ 
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
